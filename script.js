87b631abfcf35501f8531878b6ead650748fea9b
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2017-08-27 19:05:16 -0400

    add google to employment, update resume pdf, simplify websites section, fix js formatting

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ $(document).ready(function () {
 	}
 
 	window.setInterval(function(){
-  		$('.particle').each(function () {
-  			if ($(this).offset().top - $('#particle-container').offset().top > $('#particle-container').height()) {
-  				$(this).remove();
-  				$('#particle-container').append('<div class="particle" style="top: -'+Math.random()*100+'%; left: '+Math.random()*100+'%; color: '+colors[Math.floor(Math.random()*(3))]+';">v</div>');
-  			}
-  		});
+		$('.particle').each(function () {
+			if ($(this).offset().top - $('#particle-container').offset().top > $('#particle-container').height()) {
+				$(this).remove();
+				$('#particle-container').append('<div class="particle" style="top: -'+Math.random()*100+'%; left: '+Math.random()*100+'%; color: '+colors[Math.floor(Math.random()*(3))]+';">v</div>');
+			}
+		});
 	}, 1000);
 
-    var resumeExpanded = false;
+  var resumeExpanded = false;
 
-    $('#resume-button').click(function () {
-        if (!resumeExpanded) {
-            $('#resume-button').text('— Hide Details');
-            $('.pwide').addClass('resume-expanded').slideDown();
-            resumeExpanded = true;
-        }
-        else {
-            $('#resume-button').text('+ Show Details');
-            resumeExpanded = false;
-            $('.pwide').removeClass('resume-expanded').slideUp();
-        }
-    });
+  $('#resume-button').click(function () {
+    if (!resumeExpanded) {
+      $('#resume-button').text('— Hide Details');
+      $('.pwide').addClass('resume-expanded').slideDown();
+      resumeExpanded = true;
+    }
+    else {
+      $('#resume-button').text('+ Show Details');
+      resumeExpanded = false;
+      $('.pwide').removeClass('resume-expanded').slideUp();
+    }
+  });
 
 	$('.website').click(function () {
 		$(this).addClass('website-selected');
-        $('.website-note').text('Close');
+    $('.website-note').text('Close');
 	});
 
-    $('.website-note').click(function (ev) {
-        ev.stopPropagation();
-        $('.website').removeClass('website-selected');
-        $('.website-note').text('Learn more');
-    })
+  $('.website-note').click(function (ev) {
+    ev.stopPropagation();
+    $('.website').removeClass('website-selected');
+    $('.website-note').text('Learn more');
+  })
 });
